741a8b95aa570cd491548a28f872f4ba32d2a313
laurensart <artoflaurens@gmail.com>
2024-05-12 00:00:57 +0200

    fix

--- main.js
+++ main.js
@@ -3,10 +3,3 @@ import { DragGesture } from '@use-gesture/vanilla';
 import App from './App';
 
 const app = new App();
-
-const el = document.querySelector('#canvas_main');
-el.style.touchAction = 'none';
-
-const gesture = new DragGesture(el, (state) => {
-  app.onDrag(state);
-});
